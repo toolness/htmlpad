8707ae2af80fc38857480ca02195cc8008c39539
Atul Varma <varmaa@gmail.com>
2011-04-12 15:04:49 -0700

    Added some comments to bootstrap() function in manage.py.

--- manage.py
+++ manage.py
@@ -8,10 +8,17 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 path = lambda *a: os.path.join(ROOT, *a)
 
 def bootstrap():
+    '''
+    This sets up a "secret" virtualenv and installs all our dependencies
+    into it.
+    '''
+
     vdir = path('.virtualenv')
     if not os.path.exists(vdir):
         subprocess.check_call([sys.executable, path('dev', 'virtualenv.py'),
                                vdir])
+
+    # See if our dependencies changed since we last looked at them.
     requirements = path('requirements.txt')
     req_contents = open(requirements, 'r').read()
     last_requirements = path(vdir, 'requirements.txt')
@@ -20,8 +27,13 @@ def bootstrap():
         last_req_contents = open(last_requirements, 'r').read()
     if req_contents != last_req_contents:
         pip = path(vdir, 'bin', 'pip')
+        
+        # Install the dependencies with pip.
         subprocess.check_call([pip, 'install', '-r', requirements])
+
         open(last_requirements, 'w').write(req_contents)
+
+    # Activate the virtualenv. (Code taken from virtualenv docs.)
     activate_this = path(vdir, 'bin', 'activate_this.py')
     execfile(activate_this, dict(__file__=activate_this))
 
