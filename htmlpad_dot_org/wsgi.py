ff4915b9d029ba441dfcde2742d62b7a2fa4dceb
Atul Varma <varmaa@gmail.com>
2014-01-30 21:17:40 -0500

    blarg

--- htmlpad_dot_org/wsgi.py
+++ htmlpad_dot_org/wsgi.py
@@ -1,5 +1,12 @@
 # manage adds more directories to the Python path.
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import manage
 
 import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from dj_static import Cling
+
+application = Cling(django.core.handlers.wsgi.WSGIHandler())
